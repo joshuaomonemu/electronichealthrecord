bcb91eff18d76dbb9aed6a3bcb202ebf4632cadd
joshua omonemu <joshuaomonemu@gmail.com>
2025-05-15 14:42:18 +0100

    fixed all predictions

--- main/connect/db.php
+++ main/connect/db.php
@@ -1,36 +1,36 @@
 <?php
 
-                	$servername = "localhost";
-					$dbusername = "root";
-					$dbpassword = "";
-					$dbname = "adoghe";
+                	// $servername = "localhost";
+					// $dbusername = "root";
+					// $dbpassword = "";
+					// $dbname = "adoghe";
 
-					$con = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+					// $con = new mysqli($servername, $dbusername, $dbpassword, $dbname);
 
-					if ($con->connect_error) {
-					die("Connection failed: " . $con->connect_error);
-					}
+					// if ($con->connect_error) {
+					// die("Connection failed: " . $con->connect_error);
+					// }
 ?> 
 
 <?php
-// Define connection parameters
-// $serverName = "tcp:adogheproject-server.database.windows.net,1433";
-// $connectionOptions = array(
-//     "UID" => "adoghe",
-//     "PWD" => "{your_password_here}",  // Replace with your actual password
-//     "Database" => "adoghe-project",
-//     "LoginTimeout" => 30,
-//     "Encrypt" => 1,
-//     "TrustServerCertificate" => 0
-// );
+//Define connection parameters
+$serverName = "tcp:adogheproject-server.database.windows.net,1433";
+$connectionOptions = array(
+    "UID" => "adoghe",
+    "PWD" => "{your_password_here}",  // Replace with your actual password
+    "Database" => "adoghe-project",
+    "LoginTimeout" => 30,
+    "Encrypt" => 1,
+    "TrustServerCertificate" => 0
+);
 
-// // Attempt to connect to SQL Server
-// $conn = sqlsrv_connect($serverName, $connectionOptions);
+// Attempt to connect to SQL Server
+$con = sqlsrv_connect($serverName, $connectionOptions);
 
-// if ($conn === false) {
-//     echo "Error connecting to SQL Server.<br>";
-//     die(print_r(sqlsrv_errors(), true));
-// }
+if ($con === false) {
+    echo "Error connecting to SQL Server.<br>";
+    die(print_r(sqlsrv_errors(), true));
+}
 
 // You can now use $conn to execute queries
 ?> 
